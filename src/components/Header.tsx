18647681ee481a4c77646f83446aed6a4982ed94
Giovana Farinha <giovanafarinha8@gmail.com>
2025-11-27 16:54:38 +0100

    Feat: Added React Router, update 6 files, delete 3 files and create 6 files

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import "../App.css";
+
+export default function Header() {
+  return (
+    <>
+      <h1> something here</h1>
+      {/* <img src="" alt="" />  */}
+    </>
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "Arial" }}>
+      <h1> this is the homepage</h1>
+      <p> here im going to..</p>
+    </div>
+  );
+}
