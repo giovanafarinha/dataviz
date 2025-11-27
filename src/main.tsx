a26643fc85aa1309c5865abd3b97c0f9c2ea074f
Theufyr Huhwhat <204263008+Theufyr@users.noreply.github.com>
2025-11-27 17:33:23 +0100

    feat :  API query / update 4 files and create 3 files

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+// paramétrage du QueryClient de TanStack
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
