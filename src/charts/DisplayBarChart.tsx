0161d2d2e73bd59b86c75d39c7519d301e23089e
Giovana Farinha <giovanafarinha8@gmail.com>
2025-12-08 18:01:56 +0100

    update 19 files

--- src/charts/DisplayBarChart.tsx
+++ src/charts/DisplayBarChart.tsx
@@ -1,22 +1,45 @@
 import { useState } from "react";
-import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, type TooltipContentProps } from 'recharts';
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  type TooltipContentProps,
+} from "recharts";
 import { type chartsDatabase } from "../types/chartsTypes";
 
-export default function DisplayBarChart ({ chartDatas }: chartsDatabase) {
+export default function DisplayBarChart({ chartDatas }: chartsDatabase) {
   const [selectedData, setSelectedData] = useState("percent");
-  const displayPercent = (selectedData == "percent") ? true : false;
+  const displayPercent = selectedData == "percent" ? true : false;
   const percent = "Poucentage de tournages";
   const nb = "Nombre de tournages";
-  const dataKeyBar = (displayPercent) ? percent : nb;
-  const CustomTooltip = ({ active, payload, label }: TooltipContentProps<string | number, string>) => {
+  const dataKeyBar = displayPercent ? percent : nb;
+  const CustomTooltip = ({
+    active,
+    payload,
+    label,
+  }: TooltipContentProps<string | number, string>) => {
     const isVisible = active && payload && payload.length;
     return (
-      <div className="p-3 text-center bg-white border-1 border-solid border-gray-300" style={{ visibility: isVisible ? 'visible' : 'hidden' }}>
+      <div
+        className="p-3 text-center bg-white border-1 border-solid border-gray-300"
+        style={{ visibility: isVisible ? "visible" : "hidden" }}
+      >
         {isVisible && (
           <>
             <p className="label">{label}</p>
-            <p className="desc text-gray-950" style={{ display: !displayPercent ? 'inherit' : 'none' }}>{`${payload[0].value} tournages`}</p>
-            <p className="desc text-gray-400" style={{ display: displayPercent ? 'inherit' : 'none' }}>{`${payload[0].value}%`}</p>
+            <p
+              className="desc text-gray-950"
+              style={{ display: !displayPercent ? "inherit" : "none" }}
+            >{`${payload[0].value} tournages`}</p>
+            <p
+              className="desc text-gray-400"
+              style={{ display: displayPercent ? "inherit" : "none" }}
+            >{`${payload[0].value}%`}</p>
           </>
         )}
       </div>
@@ -24,12 +47,20 @@ export default function DisplayBarChart ({ chartDatas }: chartsDatabase) {
   };
   return (
     <div>
-      <select value={selectedData} onChange={(e) => setSelectedData(e.target.value)}>
+      <select
+        value={selectedData}
+        onChange={(e) => setSelectedData(e.target.value)}
+      >
         <option value="percent">{percent}</option>
         <option value="nb">{nb}</option>
       </select>
       <BarChart
-        style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
+        style={{
+          width: "100%",
+          maxWidth: "700px",
+          maxHeight: "70vh",
+          aspectRatio: 1.618,
+        }}
         responsive
         data={chartDatas}
         margin={{
@@ -44,8 +75,12 @@ export default function DisplayBarChart ({ chartDatas }: chartsDatabase) {
         <YAxis width="auto" />
         <Tooltip content={CustomTooltip} />
         <Legend />
-        <Bar dataKey={dataKeyBar} fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+        <Bar
+          dataKey={dataKeyBar}
+          fill="#8884d8"
+          activeBar={<Rectangle fill="pink" stroke="blue" />}
+        />
       </BarChart>
     </div>
   );
-};
+}
