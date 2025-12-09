1341f4aa5e5553cfedbfc9e378eae84856d37d74
Giovana Farinha <giovanafarinha8@gmail.com>
2025-12-09 14:49:03 +0100

    Merge branch 'dev' of https: update 7 files //github.com/giovanafarinha/dataviz into dev

--- src/pages/Analysis.tsx
+++ src/pages/Analysis.tsx
@@ -4,10 +4,12 @@ import Charts from "../charts/Charts";
 export default function Analysis() {
   return (
     <>
-      <h1> this is the the graphics</h1>
-      {chartChoices.map((choice, index) => (
-        <Charts key={index} chosenChart={choice} />
-      ))}
+      <h1 className="text-3xl font-semibold mt-15 m-5 -mb-5 "> Analyse des tournages à Paris</h1>
+      <span className="m-15 grid grid-cols-2 gap-10 border-1 px-5 py-5 border-black rounded-lg">
+        {chartChoices.map((choice, index) => (
+          <Charts key={index} chosenChart={choice} />
+        ))}
+      </span>
     </>
   );
 }
